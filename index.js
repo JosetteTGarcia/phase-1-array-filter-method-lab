9dcbf90189b498c75cafd4f9f4afa4330daf0fc1
Josette Garcia <josettetgarcia@gmail.com>
2022-05-08 18:28:57 -0500

    commit tree d6afac634614649c2c299a017ccbec8c78e60552

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function fuzzyMatch(array, str) {
+  const newArray = array.filter(
+    (record) =>
+      record.toLowerCase().startsWith(str.toLowerCase())
+  );
+
+  return newArray
+}
+
+function matchName(array, input) {
+  const newArray = array.filter((record) => record.name === input);
+  return newArray
+}
